eaffd8f35a90edfeb55f7ddb19e222b0d8b1592f
Katelynn Campbell-Bishop <klyncamp23@gmail.com>
2019-08-01 09:53:22 -0700

    Section1

---
+++ TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //arrange
+            var reservation = new Reservation();
+            //act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_MadeByUser_ReturnsTrue()
+        {
+            var user = new User();
+            //arrange
+            var reservation = new Reservation { MadeBy = user };
+            //act
+            var result = reservation.CanBeCancelledBy(user);
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUser_ReturnFalse()
+        {
+            //var user = new User();
+            //arrange
+            var reservation = new Reservation { MadeBy = new User() };
+            //act
+            var result = reservation.CanBeCancelledBy(new User());
+            //assert
+            Assert.IsFalse(result);
+        }
+    }
+}
