21eb5099f3e4310a8729fb3f48509f6be26ac882
Katelynn Campbell-Bishop <klyncamp23@gmail.com>
2019-08-03 08:01:31 -0700

    Section4

---
+++ TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
+        {
+            var buzz = new FizzBuzz();
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3_ReturnsFizz()
+        {
+            var buzz = new FizzBuzz();
+            var result = FizzBuzz.GetOutput(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5_ReturnsBuzz()
+        {
+            var buzz = new FizzBuzz();
+            var result = FizzBuzz.GetOutput(5);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsNotDivisibleBy3And5_ReturnTheSameNumber()
+        {
+            var buzz = new FizzBuzz();
+            var result = FizzBuzz.GetOutput(1);
+            Assert.That(result, Is.EqualTo("1"));
+        }
+    }
+}
