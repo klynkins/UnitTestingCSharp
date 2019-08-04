36587f76ed032b687fc015e98596133cb5bc9c4b
Katelynn Campbell-Bishop <klyncamp23@gmail.com>
2019-08-04 08:45:41 -0700

    Section4

---
+++ TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTest
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
+        {
+            var calculate = new DemeritPointsCalculator();
+            Assert.That(() => calculate.CalculateDemeritPoints(speed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedResult)
+        {
+            var calculate = new DemeritPointsCalculator();
+            var points = calculate.CalculateDemeritPoints(speed);
+            Assert.That(points, Is.EqualTo(expectedResult));
+        }
+    }
+}
