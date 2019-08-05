659a61b4dcf54a9c71b77a03a85b05d85d2d8d11
Katelynn Campbell-Bishop <klyncamp23@gmail.com>
2019-08-05 10:30:01 -0700

    Section4

---
+++ TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Push_ArgIsNull_ThrowArgNullException()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+        [Test]
+        public void Push_ValidArg_AddTheObjectToTheStack()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+        [Test]
+        public void Pop_StackWithAFewObjects_RemoveObjectFromTheTop()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+            stack.Pop();
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+        [Test]
+        public void Peep_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+        [Test]
+        public void Peep_StackWithObjects_ReturnObjectOnTopOfTheStack()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+            var result = stack.Peek();
+            Assert.That(result, Is.EqualTo("c"));
+        }
+        [Test]
+        public void Peep_StackWithObjects_DoesNotRemoveObjectOnTopOfTheStack()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+            stack.Peek();
+            Assert.That(stack.Count, Is.EqualTo(3));
+        }
+    }
+}
